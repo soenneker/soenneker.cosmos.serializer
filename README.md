39d1b9b2f221ede8c76673416f3fdc3ff8b89c75
Jake Soenneker <jake@soenneker.com>
2023-03-28 20:32:53 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Cosmos.Serializer.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Cosmos.Serializer/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.cosmos.serializer/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.cosmos.serializer/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Cosmos.Serializer.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Cosmos.Serializer/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Cosmos.Serializer
+### A fast, lightweight JSON (de)serializer for Azure Cosmos DB
+
+## Installation
+
+```
+Install-Package Soenneker.Cosmos.Serializer
+```
+
+## Usage
+
+When constructing a `CosmosClientOptions`, simply set the `Serializer` property to a new instance and use your [`IMemoryStreamUtil`](https://github.com/soenneker/soenneker.utils.memorystream) singleton.
+
+```csharp
+var clientOptions = new CosmosClientOptions()
+{
+    Serializer = new CosmosSystemTextJsonSerializer(_memoryStreamUtil)
+};
+```
---
+++ src/CosmosSystemTextJsonSerializer.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using Azure.Core.Serialization;
+using Microsoft.Azure.Cosmos;
+using Soenneker.Extensions.Stream;
+using Soenneker.Json.OptionsCollection;
+using Soenneker.Utils.MemoryStream.Abstract;
+
+namespace Soenneker.Cosmos.Serializer;
+
+/// <summary>
+/// Uses <see cref="JsonObjectSerializer"/> which leverages Systems.Text.Json providing a simple API to interact with on the Azure SDKs.
+/// </summary>
+public class CosmosSystemTextJsonSerializer : CosmosSerializer
+{
+    private readonly JsonObjectSerializer _systemTextJsonSerializer;
+    private readonly IMemoryStreamUtil _memoryStreamUtil;
+
+    public CosmosSystemTextJsonSerializer(IMemoryStreamUtil memoryStreamUtil)
+    {
+        _systemTextJsonSerializer = new JsonObjectSerializer(JsonOptionsCollection.WebOptions); // TODO: get more strict for performance
+        _memoryStreamUtil = memoryStreamUtil;
+    }
+
+    public override T FromStream<T>(Stream stream)
+    {
+        using (stream)
+        {
+            if (stream.CanSeek && stream.Length == 0)
+            {
+                return default!;
+            }
+
+            if (typeof(Stream).IsAssignableFrom(typeof(T)))
+            {
+                return (T) (object) stream;
+            }
+
+            return (T) _systemTextJsonSerializer.Deserialize(stream, typeof(T), default)!; // TODO: cancellationToken
+        }
+    }
+
+    public override Stream ToStream<T>(T input)
+    {
+        MemoryStream streamPayload = _memoryStreamUtil.GetSync();
+        _systemTextJsonSerializer.Serialize(streamPayload, input, typeof(T), default); // TODO: cancellationToken
+        streamPayload.ToStart(); // Seek because Position set directly will lose the buffer: https://stackoverflow.com/a/71596118
+        return streamPayload;
+    }
+}
